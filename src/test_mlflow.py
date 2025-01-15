4a5a1226bc2e6d70d2aefe3c4a2592d1efd4dc55
Rico-febrian <ricofebrian731@gmail.com>
2025-01-15 17:31:19 +0700

    Remove unused packages

--- src/test_mlflow.py
+++ src/test_mlflow.py
@@ -4,7 +4,6 @@ from sklearn import datasets
 from mlflow.models import infer_signature
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from dotenv import load_dotenv
 
