91f9c27a50738d8ccca9c75816481d2dad1775b8
Rico-febrian <ricofebrian731@gmail.com>
2025-01-15 09:32:40 +0700

    Initial commit

---
+++ src/test_mlflow.py
@@ -0,0 +1,61 @@
+import os
+import mlflow
+from sklearn import datasets
+from mlflow.models import infer_signature
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+
+if __name__ == "__main__":
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment("Flower Classification")
+    
+    # Data preparation
+    X, y = datasets.load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size = 0.2
+    )
+    
+    # EDA
+    # Preprocessing
+    # Feature Engineering
+    
+    # Training
+    params = {
+        # "solver": "lbfgs",
+        "max_iter": 100,
+        # "multi_class": "auto",
+        "random_state": 2,
+    }
+    lr = LogisticRegression(**params)
+    lr.fit(
+        X_train,
+        y_train
+    )
+    # Evaluation
+    y_pred = lr.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    # Unit test
+    
+    # Logging mlflow
+    with mlflow.start_run(run_name = "Third run"):
+        mlflow.log_params(params)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.set_tag("Training Info", "Basic LR model for iris data")
+        signature = infer_signature(X_train, lr.predict(X_train))
+        model_info = mlflow.sklearn.log_model(
+            sk_model = lr,
+            artifact_path = "iris_model",
+            signature = signature,
+            input_example = X_train,
+            registered_model_name = "Untouch Logistic Regression",
+        )
---
+++ src/unit_test.py
@@ -0,0 +1,39 @@
+import os
+import dotenv
+import mlflow
+import requests
+
+# Load environment variables
+dotenv.load_dotenv("../.env")
+
+def test_model_availability():
+    
+    # Arrange
+    MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+    CHOSEN_MODEL = os.getenv("CHOSEN_MODEL")
+    
+    mlflow.set_tracking_uri(uri = MLFLOW_TRACKING_URI)
+    mlflow.set_experiment("Flower Classification")
+    
+    # Act
+    chosen_model = mlflow.pyfunc.load_model(f"models:Untouch Logistic Regression/{CHOSEN_MODEL}")
+    
+    # Assert
+    assert type(chosen_model) == mlflow.pyfunc.PyFuncModel
+    
+def test_predict_on_ci():
+    
+    # Arrange
+    MODEL_SERVER_IP = os.getenv("MODEL_SERVER_IP")
+    
+    data = {"x1" : 1, "x2": 1, "x3": 1, "x4" : 1}
+    
+    # Act
+    res = requests.post(MODEL_SERVER_IP, json=data)
+    
+    # Assert
+    assert res.status_code == 200
+
+    
+    
+    
